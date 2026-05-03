45f0e7459d68265e936c2c9c4ab8c9086a8812fb
davidhalla10 <davidhalla10@gmail.com>
2026-05-03 00:23:58 -0400

    Added arrays and logged the values in the arrays

---
+++ practicing-creating-arrays.js
@@ -0,0 +1,8 @@
+let itemsArr = ["Milk", "Bread", "Eggs", "Butter", "Cereal"];
+let prices = [9.99, 3.89, 14.59];
+
+console.log(itemsArr);
+console.log(prices);
+
+console.log(itemsArr[0], itemsArr[2], itemsArr[3]);
+console.log(prices[0], prices[1]);
